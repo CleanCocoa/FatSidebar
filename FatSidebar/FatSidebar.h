eb033cc88bbf110f929ddc1f7670edd3839deb57
Christian Tietze <christian.tietze@gmail.com>
2017-03-26 08:31:47 +0200

    Initial Commit

---
+++ FatSidebar/FatSidebar.h
@@ -0,0 +1,19 @@
+//
+//  FatSidebar.h
+//  FatSidebar
+//
+//  Created by Christian Tietze on 26/03/17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FatSidebar.
+FOUNDATION_EXPORT double FatSidebarVersionNumber;
+
+//! Project version string for FatSidebar.
+FOUNDATION_EXPORT const unsigned char FatSidebarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FatSidebar/PublicHeader.h>
+
+
---
+++ FatSidebarTests/FatSidebarTests.swift
@@ -0,0 +1,36 @@
+//
+//  FatSidebarTests.swift
+//  FatSidebarTests
+//
+//  Created by Christian Tietze on 26/03/17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+import XCTest
+@testable import FatSidebar
+
+class FatSidebarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
